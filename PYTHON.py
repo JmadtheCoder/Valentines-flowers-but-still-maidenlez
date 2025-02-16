52d4c0a85c942f34864e479583ae8bcb3d057f5e
JmadtheCoder <dotonjuanmiguel2006@gmail.com>
2025-02-16 12:13:54 +0800

    Add files via upload

---
+++ PYTHON.py
@@ -0,0 +1,23 @@
+DATA={
+"SECURITY": "It's important to keep your conputer secure by using antivirus software, firewalls, and other security measures",
+"TROUBLESHOOTING": "If you experience issues with your computer, you may need to troubleshoot the problem",
+"SHUTTING DOWN": "When you're ginisged using your computer, you should shut it down properly.",
+"SAVING AND BACKING UP": "It's important to save and back up your data regularly to avoid data loss. ",
+"COMMUNICATE WITH OTHER DEVICES": "You can communicate with other devices connected to you computer, such as printers, scanners, or other peripherals.",
+"CONNECTING TO THE INTERNET": "You can connect to the internet to access websites, download files, and communicate eith others online. ",
+"ACCESSING DATA": "You can access data stored on your computer or other connected devices, such as external hard drives or cloud storage services.",
+"RUNNING PROGRAMS": "Once you have logged in, you can run programs on your computer.",
+"LOGGING IN": "After booting up, you will need to log in to access your user account",
+"BOOTING UP": "This is the process of staeting up the computer by loading the Operating System(OS)",
+"MULTITASKING": "Multitasking is one of the main advantages of computers. A person can do multiple tadjs, and multiple operations at the same time",
+"Speed": "One of the most advantages of computers is their incredible speef, which helps human to finish their task in a few seconds.",
+"NAPIERS BONES": "ITS MANUALLY OPERATED AND INVETED BY JOHN NAPIER",
+"Hacker": "A person who explores and learns about computer systems, often with permission",
+"Cracker": "A person who breaks into computer systems without permission, often with malicious intent",
+
+}
+
+while True
+
+ a=input("Say:")
+ print(DATA[a])
