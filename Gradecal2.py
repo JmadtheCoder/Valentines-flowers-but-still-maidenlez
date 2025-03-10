3d186f1455a3f2d1f58fcf0eaaba6a22b3a6d705
JmadtheCoder <dotonjuanmiguel2006@gmail.com>
2025-03-10 20:01:09 +0800

    Update Gradecal2.py

--- Gradecal2.py
+++ Gradecal2.py
@@ -7,7 +7,6 @@ print("""
 """)
 
 def assign_grade(score):
- 
     if score < 0 or score > 100:
         print("Invalid score! Please enter a value between 0 and 100.")
     elif score >= 90:
@@ -22,30 +21,25 @@ def assign_grade(score):
         print("Grade: F")
 
 def calculate_percentage(grades):
-   
     total_subjects = len(grades)
     total_sum = sum(grades)
-    percentage = total_sum // total_subjects
+    percentage = total_sum / total_subjects
     return percentage
 
-# List of my brainrot subjects
+# List of my customize subjects
 subjects = ["Cal2", "Rizz", "Introvert mode", "Depression", "Economic", "Programming", "Physics", "Women", "IQ"]
 
 grades = []
-print("Enter your grades for the following subjects (whole numbers only):")
+print("Enter your grades for the following subjects:")
 
 for subject in subjects:
     while True:
-        try:
-            grade = int(input(f"{subject} grade: "))
-            if 0 <= grade <= 100:
-                grades.append(grade)
-                break
-            else:
-                print("Please enter a valid grade between 0 and 100.")
-        except ValueError:
-            print("Invalid input! Please enter a whole number.")
+        grade = float(input(f"{subject} grade (0-100): "))
+        if 0 <= grade <= 100:
+            grades.append(grade)
+            break
+        else:
+            print("Please enter a valid grade between 0 and 100.")
 
 percentage = calculate_percentage(grades)
-
 print("\nYour Grade Percentage is:", percentage, "%")
